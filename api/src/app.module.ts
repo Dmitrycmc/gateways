47a900810fd1cbc8d6fe81c6f1e11f54d4e02975
Dmitry Lytov <dmitrycmc@gmail.com>
2023-04-26 14:12:38 +0530

    Api code cleaned
    (cherry picked from commit 3db21adfaa05c2c6302c8336683e53cd4c60f77a)

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GatewaysModule } from './gateways/gateways.module';
 import { DevicesModule } from './devices/devices.module';
+import {ServeStaticModule} from "@nestjs/serve-static";
+import {join} from 'path';
 
 @Module({
-  imports: [GatewaysModule, DevicesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GatewaysModule,
+    DevicesModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'spa', 'dist', 'gateways-spa'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
